e7990973d270b1c93fc56d65440dae56960929d1
VANDRIANAMW <VANDRIANAMW@gmail.com>
2019-05-29 23:28:54 +0000

    commit

---
+++ to-do-list/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Input from './components/input';
+//import Navbar from './components/navbar';
+//import Jumbotron from './components/jumbotron';
+//import Paginas from './components/pagefeatures';
+
+function Aapp() {
+  return (
+    <div className="hola">
+       <input/>
+
+    </div>
+  )
+}
+
+export default Aapp;
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ to-do-list/src/componentes/input.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Input (){
+    return (
+    <div className= "formulario">
+       <input type="text" placeholder ="What nedd to be done"/>
+
+    </div>
+
+    );
+}
+
+
+
+
+
+
+export default Input
