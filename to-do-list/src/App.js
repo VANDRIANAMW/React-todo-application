d0fa991886be435985f4f07af4b58e49a8f4e856
VANDRIANAMW <VANDRIANAMW@gmail.com>
2019-05-30 23:38:05 +0000

    index

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import './App.css';
-import Input from './components/input';
+//import Input from './components/input';
 //import Navbar from './components/navbar';
 //import Jumbotron from './components/jumbotron';
 //import Paginas from './components/pagefeatures';
 
-function Aapp() {
-  return (
-    <div className="hola">
-       <input/>
+function Input (){
+    return (
+    <form class="form-inline" role="form">
+  <div class="form-group">
+    <input type="text" class="form-control"
+           placeholder="What needs to be done?"/>
+  </div>
+  </form>
 
-    </div>
-  )
+    );
 }
 
-export default Aapp;
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Input/>
     </div>
   );
 }
